a8ef383d6971429f3f82118dc9828f2ad206059f
kali-user <eric-kali-user@kali2.192.168.100.1>
2020-10-20 12:13:39 -0500

    version7

--- src/Paquete1/Main.java
+++ src/Paquete1/Main.java
@@ -2,10 +2,8 @@ package Paquete1;
 
 public class Main {
 	public static void main(String []args) {
-		//Ventana1 v = new Ventana1();
-		//v.setVisible(true);
-		VentanaHash vh = new VentanaHash();
-		vh.setVisible(true);
+		Ventana1 v = new Ventana1();
+		v.setVisible(true);
 		
 	}
 }
